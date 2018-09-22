e5c6b185c9494d8c08ddd855ad86b85f8fb95c84
VarsPtz <otz-job@mail.ru>
2018-09-22 14:43:57 +0300

    two step commit

---
+++ lesson1/js/example.js
@@ -0,0 +1,12 @@
+// let number = 12;
+// console.log(number);
+// alert(number);
+
+let answer;
+
+// Да или Нет
+answer = confirm("Есть ли вам 18 лет");
+// Развёрнутый ответ блок для введения пользователем текста
+answer = prompt("Есть ли вам 18 лет");
+
+console.log(answer);
---
+++ lesson1/js/script.js
@@ -0,0 +1,9 @@
+// let choosePets;
+// let chooseEntertainments;
+
+let getName = prompt("Как Вас зовут?");
+let choosePets = prompt("Кто Вам больше нравятся: кошки или собаки?");
+let chooseEntertainments = prompt("Что Вы любите делать вместе с Вашим домашним питомцем? (гулять, играть, купаться и т.п.)");
+console.log("Вам больше нравятся " + choosePets + ".");
+console.log("C Вашии домашним питомцем вы любите: " + chooseEntertainments + ".");
+alert(getName + ", cпасибо за Ваши ответы!");
